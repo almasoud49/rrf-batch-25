2a814fb9578414a91062fc0ea8e9d26ad11e698a
Abdullah Al Masoud <almasoud49@yahoo.com>
2025-07-14 18:15:40 +0600

    Dynamic HTML Form and Display Data in Table

--- php/class-12/index.php
+++ php/class-12/index.php
@@ -1,54 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets//css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
         <div class="header">
             <h2>Student Information</h2>
         </div>
+        <?php 
 
-        <?php
+        $name = $email = $phone= '';
+        $students= [];
 
-        $name = $email= $phone='';
-        $students = [];
+        if($_SERVER["REQUEST_METHOD"]== "POST" && isset($_POST['submit'])){
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+
+            if(session_status() == PHP_SESSION_NONE){
+                session_start();
+            }
+
+            if(!isset($_SESSION['students'])){
+                $_SESSION['students'] = [];
+            }
+            
+            if(!empty($name) && !empty($email) && !empty($phone)){
+             $newStudent = [
+                'name'=> $name,
+                'email'=> $email,
+                'phone'=> $phone,
+              ];
+
+              array_push($_SESSION['students'],$newStudent);
+            }
+
+            if(isset($_SESSION['students'])){
+                $students = $_SESSION['students'];
+            }
+        }
         
-       if($_SERVER["REQUEST_METHOD"] =="POST" && isset($_POST['submit'])){
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-
-        if(session_status()== PHP_SESSION_NONE){
-        session_start();
-
-       }
-
-       if(!isset($_SESSION['students'])){
-        $_SESSION['students'] = [];
-       }
-       
-       if(!empty($name) && !empty($email) && !empty($phone)){
-        $newStudent = [
-        'name' => $name,
-        'email'=> $email,
-        'phone'=> $phone
-       ];
-
-       array_push($_SESSION['students'], $newStudent);
-       }
-
-       }
-
-       if(isset($_SESSION['students'])){
-        $students = $_SESSION['students'];
-       }
-
-
         ?>
+
         <div class="form-container">
             <form method="POST" action="">
                 <div class="form-group">
@@ -64,8 +63,8 @@
                     <input type="text" name="phone" id="phone" required>
                 </div>
                 <button type=" submit" name="submit" class="submit-btn">
-                Submit
-            </button>
+                    Submit
+                </button>
             </form>
         </div>
         <div class="header">
@@ -81,27 +80,29 @@
             </thead>
             <tbody>
                 <?php if(empty($students)): ?>
-                    <tr>
-                        <td>Student Name 2</td>
-                        <td>student2@gmail.com</td>
-                        <td>234-567-8901</td>
-                    </tr>
-                    <tr>
-                        <td>Student Name 3</td>
-                        <td>student3@gmail.com</td>
-                        <td>234-567-8901</td>
-                    </tr>
+                 <tr>
+                    <td>Student Name 2</td>
+                    <td>student2@gmail.com</td>
+                    <td>234-567-8901</td>
+                </tr>
+                <tr>
+                    <td>Student Name 3</td>
+                    <td>student3@gmail.com</td>
+                    <td>234-567-8901</td>
+                </tr>
                 <?php else: ?>
-                    <?php foreach($students as $student): ?>
-                    <tr>
-                        <td><?php echo $student['name']; ?></td>
-                        <td><?php echo $student['email']; ?></td>
-                        <td><?php echo $student['phone']; ?></td>
-                    </tr>
-                     <?php endforeach; ?>
-                <?php endif; ?>
+                <?php foreach($students as $student): ?>
+                <tr>
+                    <td><?php echo $student['name'] ?></td>
+                    <td><?php echo $student['email'] ?></td>
+                    <td><?php echo $student['phone'] ?></td>
+                </tr>
+                <?php endforeach; ?>
+                <?php endif;?>              
+               
             </tbody>
         </table>
     </div>
 </body>
+
 </html>
