12b95d02ac60c3168a6736b1da45c49f2ca8fa5f
Abdullah Al Masoud <almasoud49@yahoo.com>
2025-07-16 10:42:29 +0600

    Null Coalescing, Ternary, Spaceship Operator

---
+++ php/class-03/string.php
@@ -0,0 +1,10 @@
+<?php
+$firstName = "Abdullah";
+$lastName =  "Al Masoud";
+$fullName = $firstName . " " . $lastName;
+echo $fullName ."\n";
+
+$product = "Laptop";
+$price = 85000;
+
+echo "Product Name:" . $product . ". Product Price:" . $price . " " . "BDT\n";
