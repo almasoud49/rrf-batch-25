c67905d77f8e22ef2c2c19fd8f23f2e4f2f6c3d7
Abdullah Al Masoud <almasoud49@yahoo.com>
2025-07-19 22:03:10 +0600

    How to Make Logo and Menu Dynamic in WordPress Custom theme

---
+++ themes/lesson/class-05/functions.php
@@ -0,0 +1,45 @@
+<?php
+
+// Enqueue/Link scripts and style
+function lessonlms_theme_scripts()
+{
+    // Google Fonts
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Noto+Sans+Bengali:wght@100..900&family=Poppins:wght@400;700&family=Sen:wght@400..800&display=swap', null);
+
+    // Theme Style CSS
+    wp_enqueue_style('styles-css', get_template_directory_uri() . '/assets/css/style.css');
+
+    // Responsive CSS
+    wp_enqueue_style('responsive-css', get_template_directory_uri() . '/assets/css/responsive.css');
+
+    // Slick Slider CSS
+    wp_enqueue_style('slick-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css', array(), '1.9.0');
+
+    // Boxicons
+    wp_enqueue_style('boxicon-css', 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css', array(), '2.1.4');
+
+    // Default Theme Stylesheet (style.css)
+    wp_enqueue_style('lesson-theme-style', get_stylesheet_uri());
+
+    // jQuery (included in WordPress)
+    wp_enqueue_script('jquery');
+
+    // Slick JS
+    wp_enqueue_script('slick-js', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js', array('jquery'), '1.9.0', true);
+
+    // Main Theme JS
+    wp_enqueue_script('main-js', get_template_directory_uri() . "/assets/js/script.js", array('jquery'), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'lessonlms_theme_scripts');
+
+function lessonlms_theme_registration(){
+    add_theme_support('custom-logo',array(
+        'height'=> 30,
+        'weight'=> 80
+    ));
+    register_nav_menus(array(
+        'primary_menu' => __('Primary Meny','lessonlms'),
+        'mobile_menu' => __('Mobile Meny','lessonlms')
+    ));
+}
+add_action('after_setup_theme','lessonlms_theme_registration');
