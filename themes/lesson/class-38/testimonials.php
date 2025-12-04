b6d9a5271098a19196011e351d83a33109260313
Abdullah Al Masoud <almasoud49@yahoo.com>
2025-12-04 22:51:25 +0600

    Create Testimonial Custom Post Type with Custom Meta Field

---
+++ themes/lesson/class-38/testimonials.php
@@ -0,0 +1,34 @@
+
+<section class="testimonial">
+        <div class="container">
+            <div class="testimonial-items">
+                <!-- review 1 -->
+                <div class="testimonial-wrapper">
+                    <!-- student box -->
+                    <div class="student-details">
+                        <!-- Replaced PHP path with a placeholder image for demo purposes -->
+                        <img src="https://placehold.co/100x100/png" alt="Peter Moor">
+                        
+                        <div class="name">
+                            <h3>Peter Moor</h3>
+                            <span>Student of Web Design</span>
+                        </div>
+                    </div>
+
+                    <!-- review box -->
+                    <div class="reviews">
+                        <div class="absolute-svg">
+                            <!-- SVG Path approximated as original was truncated in screenshot -->
+                            <svg width="30" height="28" viewBox="0 0 30 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12.9168 1.23505V6.26199C12.9168 6.94407 12.3663 7.49652 11.6875 7.49652C9.26544 7.49652 7.94779 9.99114 7.76491 14.9155H11.6875C12.3662 14.9155 12.9168 15.4688 12.9168 16.1501V26.7647C12.9168 27.4465 12.3663 27.999 11.6875 27.999H1.22938C0.550795 27.999 1.90735e-06 27.4459 1.90735e-06 26.7647V16.15C1.90735e-06 13.7896 0.237282 11.6235 0.707131 9.71055C1.1816 7.74949 1.91613 6.035 2.8858 4.61422C3.88306 3.15417 5.13161 2.00845 6.59457 1.21054C8.06863 0.407543 9.78211 0 11.688 0C12.3662 0 12.9168 0.553486 12.9168 1.23505ZM28.771 7.49696C29.4496 7.49696 30 6.94407 30 6.26294V1.23497C30 0.553488 29.4496 0.000951767 28.771 0.000951767C26.8643 0.000951767 25.1498 0.408647 23.6757 1.21204C22.2136 2.01046 20.966 3.15655 19.9698 4.6166C19.001 6.03719 18.2673 7.75128 17.7937 9.71192C17.3248 11.6245 17.0882 13.79 17.0882 16.15V26.7647C17.0882 27.4465 17.6387 27.999 18.3175 27.999H28.7706C29.4492 27.999 30 27.4465 30 26.7647V16.1501C30 15.4688 29.4496 14.9155 28.771 14.9155H24.8519C25.0346 9.99182 26.3516 7.49696 28.771 7.49696Z" fill="#333"/>
+                            </svg>
+                        </div>
+                        <!-- Mock text added to show layout -->
+                        <p class="review-text">
+                            "The web design course has been an incredible journey. I've learned so much about responsive layouts and modern CSS techniques."
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
